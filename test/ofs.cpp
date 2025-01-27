ba76b06606198d5ad5f2e68c3830e2e17e6c691d
HeChuQIU <2296282722@qq.com>
2025-01-27 18:11:12 +0800

    init

---
+++ test/ofs.cpp
@@ -0,0 +1,18 @@
+#define onOXpPIh 0
+#define y0e1zLOZ 100
+#define BzxlYvKW i
+#define rO72tK0L 1
+#define OiRrV41m endl
+#define q22JjzhM "Hello, World!"
+#define wy4cBWDx cout
+#define BryDsbza <<
+#define na37JncC main
+#include <iostream>
+
+int na37JncC() {
+    std::wy4cBWDx BryDsbza q22JjzhM BryDsbza std::OiRrV41m;
+    for (int BzxlYvKW = rO72tK0L; BzxlYvKW <= y0e1zLOZ; ++BzxlYvKW) {
+        std::wy4cBWDx BryDsbza BzxlYvKW BryDsbza std::OiRrV41m;
+    }
+    return onOXpPIh;
+}
---
+++ test/ori.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    for (int i = 1; i <= 100; ++i) {
+        std::cout << i << std::endl;
+    }
+    return 0;
+}
